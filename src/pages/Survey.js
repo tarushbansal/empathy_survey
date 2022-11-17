fda41ec71960a7d1f0a9e6ef7ff3ee54e379c47c
Tarush Bansal <tarush.bansal@arm.com>
2022-11-17 02:04:01 +0000

    front-end api endpoints implemented

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -3,31 +3,24 @@ import MyNavbar from "../components/MyNavbar";
 import Intro from "../components/Intro";
 import Container from "react-bootstrap/Container";
 import Scale from "../components/Scale";
+import { Amplify, API } from "aws-amplify";
+import config from "../aws-exports";
+
+Amplify.configure(config);
 
 export default class Survey extends Component {
   constructor(props) {
     super(props);
-    // Fetch survey samples from API here and assign default ratings for each sampleid
-    this.samples = {
-      0: {
-        context: ["hi", "hey", "how are you"],
-        resA: "good",
-        resB: "nice",
-      },
-      1: {
-        context: ["hi", "hey", "how are you"],
-        resA: "good",
-        resB: "nice",
-      },
-    };
-    this.ratings = {
-      0: [0, 0, 0],
-      1: [0, 0, 0],
-    };
+    this.samples = API.get("surveyapi", "/samples").then(() => {
+      this.ratings = this.samples.map((sample) => ({
+        id: sample.id,
+        ratings: [0, 0, 0],
+      }));
+    });
   }
 
   submitResponses() {
-    // Post survey responses to API here
+    API.post("surveyapi", "/surveyratings", { body: this.ratings });
   }
 
   render() {
@@ -35,16 +28,16 @@ export default class Survey extends Component {
       <Container>
         <MyNavbar />
         <Intro />
-        {Object.keys(this.samples).map((id, sampleNum) => (
-          <div key={id} style={{ marginTop: 50 }}>
+        {Object.keys(this.samples).map((sample, idx) => (
+          <div key={idx} style={{ marginTop: 50 }}>
             <p>
               <strong style={{ textDecoration: "underline" }}>
-                SAMPLE {sampleNum + 1}
+                SAMPLE {idx + 1}
               </strong>
               <br />
             </p>
             <p>
-              {this.samples[id].context.map((utt, idx) => (
+              {sample.context.map((utt, idx) => (
                 <span key={idx}>
                   <strong>{idx % 2 === 0 ? "Speaker: " : "Listener: "}</strong>
                   {utt} <br />
@@ -53,21 +46,21 @@ export default class Survey extends Component {
             </p>
             <p>
               <strong>Response A: </strong>
-              {this.samples[id].resA} <br />
+              {sample.responseA} <br />
               <strong>Response B: </strong>
-              {this.samples[id].resB}
+              {sample.responseB}
             </p>
             <div style={{ marginTop: 40 }}>
               <strong>Which response is better in terms of Empathy?</strong>
-              <Scale onChange={(val) => (this.ratings[id][0] = val)} />
+              <Scale onChange={(val) => (this.ratings[sample.id][0] = val)} />
             </div>
             <div style={{ marginTop: 40 }}>
               <strong>Which response is better in terms of Relevance?</strong>
-              <Scale onChange={(val) => (this.ratings[id][1] = val)} />
+              <Scale onChange={(val) => (this.ratings[sample.id][1] = val)} />
             </div>
             <div style={{ marginTop: 40 }}>
               <strong>Which response is better in terms of Fluency?</strong>
-              <Scale onChange={(val) => (this.ratings[id][2] = val)} />
+              <Scale onChange={(val) => (this.ratings[sample.id][2] = val)} />
             </div>
           </div>
         ))}
